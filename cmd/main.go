fd7d6ae92dd50a02e0648b2e557ef51896bcf0ea
sashindionicus <searlashorn@gmail.com>
2020-10-06 19:00:32 +0300

    added server implementation

--- cmd/main.go
+++ cmd/main.go
@@ -1,5 +1,13 @@
 package main
 
+import (
+	todo "github.com/aorybalov/todo_app"
+	"log"
+)
+
 func main()  {
-	
+	srv :=new(todo.Server)
+	if err := srv.Run("8000"); err != nil {
+		log.Fatalf("error occured while running http server: %s", err.Error())
+	}
 }
